13c957d54ec7ac296d29e3da687a36d209ecee93
weil <weil@opera.com>
2022-06-23 11:10:35 +0800

    add file

---
+++ CCMUI/CCMUI/CCMUI.h
@@ -0,0 +1,18 @@
+//
+//  CCMUI.h
+//  CCMUI
+//
+//  Created by 李威 on 2022/6/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CCMUI.
+FOUNDATION_EXPORT double CCMUIVersionNumber;
+
+//! Project version string for CCMUI.
+FOUNDATION_EXPORT const unsigned char CCMUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CCMUI/PublicHeader.h>
+
+
---
+++ CCMUtils/CCMUtils/JailUtil.h
@@ -0,0 +1,17 @@
+//
+//  JailUtil.h
+//  basic_tips
+//
+//  Created by 李威 on 2022/3/12.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JailUtil : NSObject
++ (BOOL)isJail;
++ (BOOL)isJailbreak;
+@end
+
+NS_ASSUME_NONNULL_END
